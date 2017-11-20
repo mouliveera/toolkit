f2085e6816f48474d42df71d936f8b3bbb576e42
Mouli Veera <32663914+mouliveera@users.noreply.github.com>
2017-11-20 17:21:12 +0530

    Update repbiz_cluster.sh
    Updated conditions

--- repbiz_cluster.sh
+++ repbiz_cluster.sh
@@ -5,10 +5,13 @@
 #Dev_Clusterhname = dev-repbiz-backend-i-02cbe7e0646df8a92.j.r4e-dev dev-repbiz-frontend-i-083e7fa0c6e2fdd3d.j.r4e-dev
 
 rm -rf /tmp/service_status.txt.  #remove files existing before it gets executed
+while [[ $# -eq 0 ]]; do
+        echo "USAE: $0 <service_name>"
+        exit 1
+        done
+#sname=repbiz
 
-sname=repbiz
-
-#sname=$2
+sname=$1
 
 for hname in `cat /Users/mouliveera/dev_repbiz_host`;do
 
@@ -32,7 +35,7 @@ echo "$hname Service:$sname is DOWN"
 
 fi
 
-done > /tmp/service_status.txt      #Copying the result to service_status.txt file
+done >  /tmp/service_status.txt      #Copying the result to service_status.txt file
 
 ######## 
 
@@ -51,16 +54,16 @@ ndownservice=$(cat /tmp/service_status.txt |awk '{print $1 "  " $4}' |grep -i do
 ########
 
 # Condition to verify the service status list on instances
-if [[ $nhost -ne $nupservice ]];then
-	echo "Service is Down on $ndownservice instances"
+if [[ $nhost < $nupservice ]];then
+	echo "WARNING: Service is Down on $ndownservice instances"
 	exit 1
 	else
 		if [[ $nhost -eq $ndownservice ]]; then
-			echo "CRITICAL: Service is down ON all $nhost instances"
+			echo "CRITICAL: Service is down ON all instances"
 			exit 2;
 		fi
 			if [[ $nhost -eq $nupservice ]];then
-			echo "OK: Service $sname UP on all servers"
+			echo "OK: Service $sname UP on all instances"
 			exit 0
 			fi
 fi
