34fe94e3a9f0b78f95c77399125f3a8c8463c347
Mouli Veera <mouliveera@Moulis-MacBook-Pro.local>
2017-11-20 14:11:04 +0530

    repbiz_cluster

---
+++ repbiz_cluster.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# This Script is a to check service status on the cluster of servers
+
+#Dev_Clusterhname = dev-repbiz-backend-i-02cbe7e0646df8a92.j.r4e-dev dev-repbiz-frontend-i-083e7fa0c6e2fdd3d.j.r4e-dev
+
+rm -rf /tmp/service_status.txt.  #remove files existing before it gets executed
+
+sname=repbiz
+
+#sname=$2
+
+for hname in `cat /Users/mouliveera/dev_repbiz_host`;do
+
+if [ ${sname} == "repbiz" ]
+
+then
+
+nprocess=`ssh $hname "ps -ef | grep -v grep | grep repbiz | wc -l"`        
+
+if (($nprocess > 0 ));
+
+        then
+
+        echo "$hname Service:$sname is UP" 
+
+        else
+
+echo "$hname Service:$sname is DOWN"
+
+        fi
+
+fi
+
+done > /tmp/service_status.txt      #Copying the result to service_status.txt file
+
+######## 
+
+nhost=$(cat /Users/mouliveera/dev_repbiz_host|wc -l)
+upservices=$(cat /tmp/service_status.txt |awk '{print $1 "  " $4}' |grep -i up)
+
+downservices=$(cat /tmp/service_status.txt |awk '{print $1 "  " $4}' |grep -i down)
+
+########
+
+nservices=$(cat /tmp/service_status.txt |awk '{print $1 "  " $4}'|wc -l)
+
+nupservice=$(cat /tmp/service_status.txt |awk '{print $1 "  " $4}' |grep -i up |wc -l) # check number of UP processing servers
+
+ndownservice=$(cat /tmp/service_status.txt |awk '{print $1 "  " $4}' |grep -i down |wc -l) # check number of DOWN processing servers
+########
+
+# Condition to verify the service status list on instances
+if [[ $nhost -ne $nupservice ]];then
+	echo "Service is Down on $ndownservice instances"
+	exit 1
+	else
+		if [[ $nhost -eq $ndownservice ]]; then
+			echo "CRITICAL: Service is down ON all $nhost instances"
+			exit 2;
+		fi
+			if [[ $nhost -eq $nupservice ]];then
+			echo "OK: Service $sname UP on all servers"
+			exit 0
+			fi
+fi
+	
+#End
